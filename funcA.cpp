1519db4e80b333ed18b6811d1d021400464f26a4
Artem Puzenko <a.puzenko2004@gmail.com>
2024-09-29 12:47:22 +0300

    Initial implementation: FuncA returns 0

---
+++ funcA.cpp
@@ -0,0 +1,6 @@
+#include "funcA.h"
+#include <cmath>
+
+double FuncA::compute(double x) {
+    return 0;
+}
---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include "funcA.h"
+#include <iostream>
+
+int main() {
+    FuncA func;
+    std::cout << "Result: " << func.compute(1.0) << std::endl;
+    return 0;
+}
